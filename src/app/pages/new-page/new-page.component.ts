28313201d361d3f03da03b688be6397e17daa005
Craig Watson <105209145+craignable@users.noreply.github.com>
2022-07-06 08:54:58 +0100

    chart and half working button

--- src/app/pages/new-page/new-page.component.ts
+++ src/app/pages/new-page/new-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { NgModule, Component, ViewChild } from '@angular/core';
 import { Customer, Service } from 'src/app/shared/services/app.service';
+import repaintFloatingActionButton from 'devextreme/ui/speed_dial_action/repaint_floating_action_button';
+import { DxDataGridModule, DxDataGridComponent, DxSpeedDialActionModule, DxSelectBoxModule, } from 'devextreme-angular';
 
 @Component({
   templateUrl: 'new-page.component.html',
@@ -7,9 +9,27 @@ import { Customer, Service } from 'src/app/shared/services/app.service';
 })
 
 export class NewPageComponent {
+
+  @ViewChild(DxDataGridComponent, { static: false }) grid: DxDataGridComponent;
   customers: Customer[];
+  selectedRowIndex = -1;
 
   constructor(service: Service) {
     this.customers = service.getCustomers();
   }
+
+  editRow() {
+    this.grid.instance.editRow(this.selectedRowIndex);
+    this.grid.instance.deselectAll();
+  }
+
+  deleteRow() {
+    this.grid.instance.deleteRow(this.selectedRowIndex);
+    this.grid.instance.deselectAll();
+  }
+
+  addRow() {
+    this.grid.instance.addRow();
+    this.grid.instance.deselectAll();
+  } 
 }
--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import data from './players.json';
 
 export class Customer {
   ID!: number;
@@ -12,79 +13,81 @@ export class Customer {
   Slams!: number;
 }
 
-const customers: Customer[] = [{
-  ID: 1,
-  Name: 'Roger Federer',
-  Age: 40,
-  Titles: 103,
-  Slams: 20,
-}, {
-  ID: 2,
-  Name: 'Rafael Nadal',
-  Age: 36,
-  Titles: 92,
-  Slams: 22,
-}, {
-    ID: 3,
-    Name: 'Serena Williams',
-    Age: 40,
-    Titles: 73,
-    Slams: 23,
-}, {
-    ID: 4,
-    Name: 'Novak Djokovic',
-    Age: 35,
-    Titles: 87,
-    Slams: 20,
-}, {
-    ID: 5,
-    Name: 'Steffi Graff',
-    Age: 53,
-    Titles: 107,
-    Slams: 22,
-  }, {
-      ID: 6,
-      Name: 'Pete Sampras',
-      Age: 50,
-      Titles: 64,
-      Slams: 14,
-  }, {
-      ID: 7,
-      Name: 'Andre Agassi',
-      Age: 52,
-      Titles: 60,
-      Slams: 8,
-  }, {
-    ID: 8,
-    Name: 'Venus Williams',
-    Age: 42,
-    Titles: 49,
-    Slams: 7,
-  }, {
-      ID: 9,
-      Name: 'Jimmy Connors',
-      Age: 69,
-      Titles: 109,
-      Slams: 8,
-  }, {
-      ID: 10,
-      Name: 'Maria Sharapova',
-      Age: 35,
-      Titles: 36,
-      Slams: 5,
-  }, {
-    ID: 11,
-    Name: 'Bjorn Borg',
-    Age: 66,
-    Titles: 66,
-    Slams: 11,
-  }, {
-      ID: 12,
-      Name: 'Martina Hingis',
-      Age: 41,
-      Titles: 43,
-      Slams: 5,
-  }];
+const customers: Customer[] = data;
+
+// const customers: Customer[] = [{
+//   ID: 1,
+//   Name: 'Roger Federer',
+//   Age: 40,
+//   Titles: 103,
+//   Slams: 20,
+// }, {
+//   ID: 2,
+//   Name: 'Rafael Nadal',
+//   Age: 36,
+//   Titles: 92,
+//   Slams: 22,
+// }, {
+//     ID: 3,
+//     Name: 'Serena Williams',
+//     Age: 40,
+//     Titles: 73,
+//     Slams: 23,
+// }, {
+//     ID: 4,
+//     Name: 'Novak Djokovic',
+//     Age: 35,
+//     Titles: 87,
+//     Slams: 20,
+// }, {
+//     ID: 5,
+//     Name: 'Steffi Graff',
+//     Age: 53,
+//     Titles: 107,
+//     Slams: 22,
+//   }, {
+//       ID: 6,
+//       Name: 'Pete Sampras',
+//       Age: 50,
+//       Titles: 64,
+//       Slams: 14,
+//   }, {
+//       ID: 7,
+//       Name: 'Andre Agassi',
+//       Age: 52,
+//       Titles: 60,
+//       Slams: 8,
+//   }, {
+//     ID: 8,
+//     Name: 'Venus Williams',
+//     Age: 42,
+//     Titles: 49,
+//     Slams: 7,
+//   }, {
+//       ID: 9,
+//       Name: 'Jimmy Connors',
+//       Age: 69,
+//       Titles: 109,
+//       Slams: 8,
+//   }, {
+//       ID: 10,
+//       Name: 'Maria Sharapova',
+//       Age: 35,
+//       Titles: 36,
+//       Slams: 5,
+//   }, {
+//     ID: 11,
+//     Name: 'Bjorn Borg',
+//     Age: 66,
+//     Titles: 66,
+//     Slams: 11,
+//   }, {
+//       ID: 12,
+//       Name: 'Martina Hingis',
+//       Age: 41,
+//       Titles: 43,
+//       Slams: 5,
+//   }];
 
 @Injectable()
 export class Service {
